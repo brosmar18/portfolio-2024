75f3e9528bd09008fda29be52cb9af93b32c725d
brosmar18 <bryangonzalez1896@icloud.com>
2024-01-26 15:37:06 -0600

    Constants added

--- components/shared/Header.tsx
+++ components/shared/Header.tsx
@@ -1,8 +1,24 @@
+import Image from 'next/image'
+import Link from 'next/link'
 import React from 'react'
 
 const Header = () => {
   return (
-    <div>Header</div>
+    <header className='w-full border-b'>
+      <div className='max-w-7xl lg:max-auto p-5 md:px-10 xl:px-0 w-full'>
+        <Link href='/' className='w-36'>
+          <Image 
+            src='/assets/images/logo.png'
+            alt='logo'
+            width={50}
+            height={50}
+          />
+        </Link>
+        <nav>
+          
+        </nav>
+      </div>
+    </header>
   )
 }
 
---
+++ constants/index.js
@@ -0,0 +1,22 @@
+export const navLinks = [
+    {
+        label: 'Home',
+        route: '/',
+    },
+    {
+        label: 'Experience',
+        rotue: '/experience',
+    },
+    {
+        label: 'Projects',
+        rotue: '/projects',
+    },
+    {
+        label: 'About Us',
+        rotue: '/about',
+    },
+    {
+        label: 'Contact',
+        rotue: '/contact',
+    },
+]

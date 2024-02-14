e01c0d65b3be1c686188a07f878d6057cef0c452
brosmar18 <bryangonzalez1896@icloud.com>
2024-02-14 17:49:47 -0600

    link clicking bug fixed

--- components/shared/Navbar.tsx
+++ components/shared/Navbar.tsx
@@ -6,6 +6,7 @@ import { usePathname } from 'next/navigation';
 import React, { useState } from 'react';
 import { FaGithub, FaLinkedin } from "react-icons/fa";
 import { motion } from 'framer-motion';
+import Image from 'next/image';
 
 
 const Navbar = () => {
@@ -68,20 +69,19 @@ const Navbar = () => {
 
   return (
     <nav className='h-full flex items-center justify-between px-4 sm:px-8 md:px-12 lg:px-20 xl:px-48 text-lg'>
-      <div className='hidden md:flex gap-4 w-1/3'>
+      <div className='hidden md:flex gap-4 w-1/3 z-40'>
         {navLinks.map(link => (
-          <Link className={`rounded p-1 ${pathname === link.route ? 'bg-black text-white' : ''}`} href={link.route} key={link.label}>
+          <Link className={`rounded p-1 ${pathname === link.route ? 'bg-white text-black' : ''}`} href={link.route} key={link.label}>
             {link.label}
           </Link>
         ))}
       </div>
       <div className="md:hidden lg:flex xl:w-1/3 xl:justify-center">
-        <Link href='/' className="text-sm rounded-md p-1 bg-black font-semibold flex items-center justify-center">
-          <span className="text-white mr-1">Bryan</span>
-          <span className="w-12 rounded bg-white text-black flex items-center justify-center">.dev</span>
+        <Link href='/' className="text-sm rounded-md p-1 bg-white font-semibold flex items-center justify-center">
+          <Image src='/assets/images/logo.png' alt='logo' width={30} height={30} />
         </Link>
       </div>
-      <div className='hidden md:flex gap-4 w-1/3'>
+      <div className='hidden md:flex items-center justify-center gap-4 w-1/3'>
         <Link href='/'>
           <FaGithub size={28} />
         </Link>
@@ -95,15 +95,15 @@ const Navbar = () => {
           <motion.div
             variants={topVariants}
             animate={open ? "opened" : "closed"}
-            className="w-10 h-1 bg-white rounded origin-left"></motion.div>
+            className="w-10 h-[3px] bg-white rounded origin-left"></motion.div>
           <motion.div
             variants={centerVariants}
             animate={open ? "opened" : "closed"}
-            className="w-10 h-1 bg-white rounded"></motion.div>
+            className="w-10 h-[3px] bg-white rounded"></motion.div>
           <motion.div
             variants={bottomVariants}
             animate={open ? "opened" : "closed"}
-            className="w-10 h-1 bg-white rounded origin-left"></motion.div>
+            className="w-10 h-[3px] bg-white rounded origin-left"></motion.div>
         </button>
         {/* NAV LINKS */}
         {open && (
@@ -111,7 +111,7 @@ const Navbar = () => {
             variants={listVariants}
             initial='closed'
             animate='opened'
-            className="absolute top-0 left-0 w-screen h-screen bg-black text-white flex flex-col items-center justify-center gap-8 text-4xl z-40"
+            className="absolute top-0 left-0 w-screen h-screen bg-black text-white flex flex-col items-center justify-center gap-8 text-4xl z-20"
           >
             {navLinks.map(link => (
               <motion.div

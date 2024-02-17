bf0a8be0da5ccd176d93a894b682426fa194da4d
brosmar18 <bryangonzalez1896@icloud.com>
2024-02-16 18:47:43 -0600

    practice card built with animated pin

---
+++ app/(root)/practice/page.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import Card from '../../../components/shared/Card';
+
+const Practice = () => {
+ return (
+  <div>
+   <Card />
+  </div>
+ )
+}
+
+export default Practice

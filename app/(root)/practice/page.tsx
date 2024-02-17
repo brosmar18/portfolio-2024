ebad21b987e09a729da3aa51369c5156c1c13418
brosmar18 <bryangonzalez1896@icloud.com>
2024-02-16 19:59:36 -0600

    card updated

--- app/(root)/practice/page.tsx
+++ app/(root)/practice/page.tsx
@@ -3,8 +3,18 @@ import Card from '../../../components/shared/Card';
 
 const Practice = () => {
  return (
-  <div>
-   <Card />
+  <div className="sm:px-16 px-6 sm:py-16 py-10 max-w-7xl mx-auto relative z-0 mt-8">
+   <div>
+    <p className='sm:text-[18px] text-[14px] text-secondary uppercase tracking-wider text-center'>
+     What I&#39;ve done
+    </p>
+    <h2 className='text-white font-black md:text-[60px] sm:text-[50px] xs:text-[40px] text-[30px] text-center'>
+     Work Experience
+    </h2>
+   </div>
+   <div className=''>
+
+   </div>
   </div>
  )
 }
--- components/shared/Card.tsx
+++ components/shared/Card.tsx
@@ -1,26 +1,8 @@
-'use client';
 import React from 'react';
-import { PinContainer } from '../ui/PinContainer';
-
-
 
 const Card = () => {
   return (
-    <div className="h-[40rem] w-full flex items-center justify-center">
-      <PinContainer
-        company='/ui.aceternity.com'
-      >
-        <div className='flex basis-full flex-col gap-4 tracking-tight sm:basis-1/2 w-[30rem] h-[30rem]'>
-          <h3 className='font-bold text-black'>Aceternity</h3>
-          <div className='font-normal text-base'>
-            <span className='text-slate-500'>
-              Customizable Tailwind CSS and Framer Motion Components
-            </span>
-          </div>
-          <div className='bg-gradient-to-br from-violet-500 via-purple-500 to-blue-500 flex flex-1 w-full rounded-lg mt-4' />
-        </div>
-      </PinContainer>
-    </div>
+    <div>Card</div>
   )
 }
 
